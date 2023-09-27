862178f69c9335fff07825dac3f14a7dd63c5603
kwonjihoo <yolohoo@naver.com>
2023-09-27 18:56:22 +0900

    0927 view 까지 끝

--- 09_서브쿼리연습문제.sql
+++ 09_서브쿼리연습문제.sql
@@ -11,7 +11,8 @@ SELECT *
 FROM employees 
 WHERE salary > (SELECT avg(salary)
                 FROM employees);
-                
+
+
 SELECT COUNT(*) AS 사원수
 FROM employees 
 WHERE salary > (SELECT avg(salary)
@@ -41,7 +42,7 @@ WHERE department_id = (SELECT department_id
 */
 SELECT *
 FROM employees
-WHERE manager_id >ALL (SELECT manager_id
+WHERE manager_id > (SELECT manager_id
             FROM employees
             WHERE first_name = 'Pat'); 
 SELECT *
@@ -181,4 +182,146 @@ SELECT
 FROM locations loc
 ORDER BY country_name ;
  
+  
+  /*
+문제 12. 
+employees테이블, departments테이블을 left조인 hire_date를 오름차순 기준으로 
+1-10번째 데이터만 출력합니다.
+조건) rownum을 적용하여 번호, 직원아이디, 이름, 전화번호, 입사일, 
+부서아이디, 부서이름 을 출력합니다.
+조건) hire_date를 기준으로 오름차순 정렬 되어야 합니다. rownum이 틀어지면 안됩니다.
+*/
+SELECT ROWNUM as rn ,tal.*
+FROM (
+    SELECT
+        e.employee_id, e.first_name, e.phone_number, 
+        e.hire_date, d.department_id, d.department_name
+    FROM employees e
+    LEFT JOIN departments d
+    ON e.department_id = d.department_id
+    ORDER BY e.hire_date 
+)tal;
+
+
+
+
+/*
+문제 13. 
+--EMPLOYEES 와 DEPARTMENTS 테이블에서 JOB_ID가 SA_MAN 사원의 \
+정보의 LAST_NAME, JOB_ID, 
+DEPARTMENT_ID,DEPARTMENT_NAME을 출력하세요.
+*/
+SELECT
+    e.last_name,
+    e.job_id,
+    (SELECT 
+        d.department_id
+    FROM departments d
+    WHERE e.department_id = d.department_id) as department_id,
+    
+    (SELECT 
+        d.department_name
+    FROM departments d
+    WHERE e.department_id = d.department_id) as department_name
+FROM employees e
+WHERE e.job_id = 'SA_MAN';
+
+
+
+
+
+/*
+문제 14
+--DEPARTMENT테이블에서 각 부서의 ID, NAME, MANAGER_ID와 부서에 속한 인원수를 출력하세요.
+--인원수 기준 내림차순 정렬하세요.
+--사람이 없는 부서는 출력하지 뽑지 않습니다.
+*/
+
+
+SELECT
+    d.department_id,
+    d.department_name,
+    d.manager_id,
+    COUNT(e.employee_id)
+FROM departments d
+LEFT JOIN employees e
+on e.department_id = d.department_id
+WHERE e.employee_id is not null
+GROUP BY d.department_name, d.department_id,d.manager_id;
+
+/*
+문제 15
+--부서에 대한 정보 전부와, 주소, 우편번호, 부서별 평균 연봉을 구해서 출력하세요.
+--부서별 평균이 없으면 0으로 출력하세요.
+*/
+
+
+
+SELECT d.*,
+   NVL((SELECT
+      avg(e.salary)
+        FROM employees e
+         WHERE e.department_id = d.department_id),0)as salary,
+        (SELECT
+                loc.postal_code
+                FROM locations loc
+                WHERE d.location_id = loc.location_id
+            )as postal_code,
+         (SELECT
+                loc.street_address
+                FROM locations loc
+                WHERE d.location_id = loc.location_id
+            )as street_address  
+      
+FROM departments d;
+
+
+
+
+
+
+
+ 
+/*
+문제 16
+-문제 15 결과에 대해 DEPARTMENT_ID기준으로 내림차순 정렬해서 
+ROWNUM을 붙여 1-10 데이터 까지만 출력하세요.
+*/
+SELECT *
+FROM (SELECT ROWNUM AS rn,tal.*
+FROM (
+SELECT d.*,
+   NVL((SELECT
+      avg(e.salary)
+        FROM employees e
+         WHERE e.department_id = d.department_id),0)as salary,
+        (SELECT
+                loc.postal_code
+                FROM locations loc
+                WHERE d.location_id = loc.location_id
+            )as postal_code,
+         (SELECT
+                loc.street_address
+                FROM locations loc
+                WHERE d.location_id = loc.location_id
+            )as street_address  
+      
+FROM departments d
+ORDER BY department_id DESC
+)tal)
+WHERE rn>=1 AND rn <=10
+
+
+  
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
     
